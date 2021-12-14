af48a8fb6657b9dfacf1787cdba0cf188ffc150e
Ismail Ahmedov <iahmedov@paradigmadigital.com>
2021-12-14 05:37:12 +0100

    Add inline implementation for Sprout Method example

--- src/main/java/com/github/ismail2ov/sproutmethod/TransactionGate.java
+++ src/main/java/com/github/ismail2ov/sproutmethod/TransactionGate.java
@@ -2,6 +2,8 @@ package com.github.ismail2ov.sproutmethod;
 
 import lombok.RequiredArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -9,9 +11,14 @@ public class TransactionGate {
     private final TransactionBundle transactionBundle;
 
     public void postEntries(List<Article> articles) {
+        List<Article> articlesToAdd = new ArrayList<>();
         for (Article article : articles) {
+            if (!transactionBundle.getArticlesManager().contains(article)) {
+                article.postDate();
+                articlesToAdd.add(article);
+            }
             article.postDate();
         }
-        transactionBundle.getArticlesManager().addAll(articles);
+        transactionBundle.getArticlesManager().addAll(articlesToAdd);
     }
 }
